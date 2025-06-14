84c7e5a742ccf76c35434a7f6a3a8f7ffd446b8b
onikshoti <onikshot@gmail.com>
2025-06-14 14:29:52 +0200

    lesson 9

---
+++ page.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+<img src="<?php header_image(); ?>" height="<?php echo get_custum_header()->height;?>" width="<?php echo get_custum_header()->width; ?>" alt="header photo">
+
+<div id="id" class="site-content">
+ <div id="primary" class="content-area">
+<main id="main"  class="site-main">
+    <div class="container">
+    <div class="page-item">
+    
+    
+
+
+
+    </div>
+
+
+    </div>
+
+
+ </div>
+
+
+</div>

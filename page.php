dbe52d6905f655fa9ac70f232be7f16d1ac6aae3
onikshoti <onikshot@gmail.com>
2025-06-21 14:15:36 +0200

    module 8

--- page.php
+++ page.php
@@ -1,24 +1,31 @@
-<?php get_header(); ?>
+<?php  get_header(); ?>
 
-<img src="<?php header_image(); ?>" height="<?php echo get_custum_header()->height;?>" width="<?php echo get_custum_header()->width; ?>" alt="header photo">
+
+<img src="<?php header_image(); ?>"  height="<?php  echo get_custom_header()->height;?>" width="<?php echo get_custom_header()->width; ?>" alt="Header photo">
+
 
 <div id="id" class="site-content">
- <div id="primary" class="content-area">
-<main id="main"  class="site-main">
-    <div class="container">
-    <div class="page-item">
-    
-    
+   <div id="primary" class="content-area">
+     <main id="main" class="site-main">
+         <div class="container">
+         <div class="page-item">
+             <?php
+            
+               while(have_posts()): the_post();
+               get_template_part('parts/content','page');
 
 
-
-    </div>
-
-
-    </div>
+               if(comments_open() || get_comments_number()) {
+                  comments_template();
+               } 
 
 
- </div>
-
-
+              endwhile;
+             ?>
+             
+         </div>
+        </div>
+     </main>
+   </div>
 </div>
+<?php  get_footer(); ?>
---
+++ wp-theme/page-about.php
@@ -0,0 +1,36 @@
+
+<?php 
+/* Template Name: About page*/
+get_header(); 
+
+?>
+
+<div id="content" class="site-content">
+    <main id="main" class="site-main">
+    <section class="about-hero">
+       <div class="container">
+    <h1>about us</h1>
+      <p> Welcome to our tech corner - where innovation meets explenation. </p>
+      </div>
+    </section>
+
+    <section class="about-intro">
+     <div class="container">
+              <h2>who we are</h2>
+              <p> we are tean if developers, engineers, and tech enthusiasts who love to write abot the fast-moving world of technology </p>
+              </div>
+                </section>
+
+                <section class="about-mission">
+                    <div class="container">
+                        <h2> our misions </h2>
+                        <p> our goal is simple: to empower minds and passionate techies by delivering accurate, useful, and accessible
+                           content.whether youre a seasonded developer or just getting started, we aim to be your go-to resource for tech nowledge.         
+                    </p>
+                    </div>
+
+                </section>
+
+    </main>
+</div>
+

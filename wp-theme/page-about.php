f2a33b4854242e2d1701d4833a8db074522a3663
Ron Shala <ron.shala17@gmail.com>
2025-06-21 14:15:00 +0200

    10

---
+++ wp-theme/page-about.php
@@ -0,0 +1,33 @@
+<?php 
+/*Template Name: About Page*/ 
+get_header(); 
+
+?>
+
+<div id="content" class="site-content">
+    <main id="main" class="site-main">
+        <section class="about-hero">
+            <div class="container">
+                <h1>About us</h1>
+                <p> Welcome to our tech corner - where inovation meets explanation. </p>
+            </div>
+        </section>
+
+        <section class="about-intro">
+            <div class="container">
+                <h2> who we are </h2>
+                <p> we are team of developers, emginiers, and tech enthusiats who love to write about the fast-moving world of technology</p>
+
+             </div>
+        </section>
+
+        <section class="about-mission">
+            <div class="container">
+                <h2> our mission</h2>
+                <p>our goal is simple: to empower minds and passionate techies by delivering accurate, useful, and accesible content.whether youre a seasonded developer or just getting started, we aim to be your go-to resource for thech nowledge.</p>
+            </div>
+        </section>
+    </main>
+</div>
+    
+    

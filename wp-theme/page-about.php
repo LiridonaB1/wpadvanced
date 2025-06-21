09dfa023e4cbec6973da2f86e36f80f50b1c2e87
Artiol Thaqi <artiolthaqi06@gmail.com>
2025-06-21 14:15:15 +0200

    Lesson 10

---
+++ wp-theme/page-about.php
@@ -0,0 +1,35 @@
+
+<?php
+/* Template Name: About Page*/
+get_header();
+ ?>
+
+<div id="content" class="site-content">
+    <main id="main" class="site-main">
+        <section class="about-hero">
+            <div class="container">
+                <h1>About Us</h1>
+                <p>Welcome to our tech corner - where innovation meets explanation. </p>
+            </div>
+        </section>
+
+        <section class="about-intro">
+            <div class="container">
+                <h2> Who We Are </h2>
+                <p>We are a team of developers, engineers, and tech enthusiasts who love to write about the fast-moving world of technology</p>
+            </div>
+        </section>
+        
+        <section class="about-mission">
+            <div class="container">
+                <h2>Our Mission</h2>
+                <p>Our goal is simple: to empower minds and passionate techies by delivering accurate, useful and accessible
+                    content. Whether you are a seasonded developer or just getting started, we aim to be your go-to resource for tech nowledge.</p>
+            </div>
+        </section>
+
+
+
+    </main>
+</div>
+

767dfb16ec04ef248067fa6953959fd519a1cd03
blendosmani <blendosmani010@gmail.com>
2025-06-21 14:16:03 +0200

    lesson 8

---
+++ wp-theme/page-about.php
@@ -0,0 +1,31 @@
+
+<?php 
+/* Template Name: About Page*/
+get_header(); 
+
+?>
+
+<div id="content" class="site-content">
+    <main id="main" class="site-main">
+        <section class="about-hero">
+            <div class="container">
+                <h1>About Us</h1>
+                <p>Welcome to uor tech corner - where innovation meets explanation. </p>
+            </div>
+        </section>
+        <section class="about-intro">
+            <div class="container">
+                <h2>Who We Are</h2>
+                <p> We are team of developers, engineers, and tech enthusiasts who love to write about the fast-moving world of technology</p>
+            </div>
+        </section>
+
+        <section class="about-mission">
+            <div class= "container">
+                <h2> Our Mission </h2>
+                <p>Our goal is simple: to empower minds and pasionate techies by delivering accurate, useful, and accessible 
+                    conntent. Whether you're a seasonded developer or just getting started, we aim tobe your go-to resource for tech knoledge.</p>
+            </div>
+        </section>
+    </main>
+</div>
--- wp-theme/page.php
+++ wp-theme/page.php
@@ -6,10 +6,22 @@
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
             <div class="container">
-            <div class="page-item">
+            <div class="page-item">                
+                <?php
                 
+                while(have_posts()): the_post();
+                get_template_parts('parts/content','page');
+                 
+                if(comments_open() || get_comments_number()){
+                    comments_template();
+                }
+
+                endwhile;
+                ?>
+
             </div>
             </div>
         </main>
     </div>
 </div>
+<?php get_footer(); ?>

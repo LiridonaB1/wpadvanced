964c13a8a7cdd819cdd0ba57c503b7470094768b
Liridona Baliu <liridona.baliu@digitalschool.tech>
2025-06-28 14:31:09 +0200

    lesson

--- wp-theme/page-about.php
+++ wp-theme/page-about.php
@@ -27,8 +27,29 @@ get_header();
                 <p>Our goal is simple: to empower curious minds and passionate techies by delivering accurate, useful, and accessible content. Whether you're a seasoned developer or just getting started, we aim to be your go-to resource for tech knowledge.</p>
             </div>
         </section>
- 
-        
+       
+        <section class="about-what-we-cover">
+           <div class="container">
+               <h2>What We Cover</h2>
+               <ul>
+                   <li>Programming tutorials and tips</li>
+                   <li>Software and app reviews</li>
+                   <li>AI, machine learning and data science insights</li>
+                   <li>Web development, DevOps , and cloud computing</li>
+                   <li>Trends in tech and industry news</li>
+               </ul>
+           </div>
+        </section>
+
+        <section class="about-contact">
+            <div class="container">
+                 <h2>Want to Connect?</h2>
+                 <p>We love hearing from our readers! Reach out to us via our <a href="/contact">Contact page</a> or follow us on socila media to stay updated.</p>
+                   
+            </div>
+        </section>
+
     </main>
 </div>
  
+<?php  get_footer();  ?>
